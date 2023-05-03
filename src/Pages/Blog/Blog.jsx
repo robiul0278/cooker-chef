4070d7706c9679d0979f71efc4cf598217209578
robiul0278 <robiul0278@gmail.com>
2023-05-03 23:34:19 +0600

    load chef json data

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-           Blog is coming ...
-        </div>
-    );
+  return (
+    <div>
+      Chef Academy students learn the art of cooking from a professional
+      platform to gain experience in the field. We offer bespoke, practical and
+      effective courses that focus not only on teaching the main cooking
+      techniques, but also teach the meaning of what it is like to be a chef
+      working in a professional kitchen. Our students have the chance to train
+      alongside London's best chefs, from top level fine dining to Michelin star
+      level, in the kitchens of their restaurants. They will be ready to start
+      working immediately after the successful completion of the course. They
+      will have all the practical and theoretical skills to confidently enter a
+      professional kitchen. Chef Academy is a prestigious school that is
+      internationally recognised. The Academy was founded in Italy and due to
+      huge success, another school was opened shortly afterwards in London,
+      which is amongst the most reputable in the UK. Graduating at Chef Academy
+      will give you access to the professional world of cooking. Our school is
+      also internationally recognized by ASIC (Accreditation Service for
+      International Colleges).
+    </div>
+  );
 };
 
 export default Blog;
