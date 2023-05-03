acf83a942a95ca4a6d749126fa9d83f7b6b0d1fc
robiul0278 <robiul0278@gmail.com>
2023-05-03 23:56:53 +0600

    chef view recipe page complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,3 +10,15 @@ export default {
   plugins: [require("daisyui")]
 }
 
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")]
+// }
+
